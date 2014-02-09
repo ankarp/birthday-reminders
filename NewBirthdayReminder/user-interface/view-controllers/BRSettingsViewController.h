f4787a7ae4f4b2a67b679a9daa0bb133dbfbf675
Anatoly Karp <ankarp1@gmail.com>
2014-02-08 17:08:18 -0800

    add BRSettingsViewController

---
+++ NewBirthdayReminder/user-interface/view-controllers/BRSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BRSettingsViewController.h
+//  NewBirthdayReminder
+//
+//  Created by Jennnifer Karp on 2/8/14.
+//  Copyright (c) 2014 tolikarp. All rights reserved.
+//
+
+#import "BRCoreViewController.h"
+
+@interface BRSettingsViewController : BRCoreViewController
+
+@end
