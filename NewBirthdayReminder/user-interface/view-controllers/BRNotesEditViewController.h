b4640331a68782f48d4514ac58d03bda39e65b3c
Anatoly Karp <ankarp1@gmail.com>
2014-02-09 13:05:11 -0800

    react to edit notes text changes

--- NewBirthdayReminder/user-interface/view-controllers/BRNotesEditViewController.h
+++ NewBirthdayReminder/user-interface/view-controllers/BRNotesEditViewController.h
@@ -8,6 +8,9 @@
 
 #import "BRCoreViewController.h"
 
-@interface BRNotesEditViewController : BRCoreViewController
+@interface BRNotesEditViewController : BRCoreViewController <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
