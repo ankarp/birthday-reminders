1bbfd6696485840a99e0c090b68cbf8b7d809219
Anatoly Karp <ankarp1@gmail.com>
2014-02-08 16:59:47 -0800

    add BRNotesEditController

---
+++ NewBirthdayReminder/user-interface/view-controllers/BRNotesEditViewController.m
@@ -0,0 +1,38 @@
+//
+//  BRNotesEditViewController.m
+//  NewBirthdayReminder
+//
+//  Created by Jennnifer Karp on 2/8/14.
+//  Copyright (c) 2014 tolikarp. All rights reserved.
+//
+
+#import "BRNotesEditViewController.h"
+
+@interface BRNotesEditViewController ()
+
+@end
+
+@implementation BRNotesEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
