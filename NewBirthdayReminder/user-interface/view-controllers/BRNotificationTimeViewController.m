902280d1ce0be332c63b916a04bebfcaf580dea6
Anatoly Karp <ankarp1@gmail.com>
2014-02-08 18:54:41 -0800

    add BRNotificationTimeViewController

---
+++ NewBirthdayReminder/user-interface/view-controllers/BRNotificationTimeViewController.m
@@ -0,0 +1,38 @@
+//
+//  BRNotificationTimeViewController.m
+//  NewBirthdayReminder
+//
+//  Created by Jennnifer Karp on 2/8/14.
+//  Copyright (c) 2014 tolikarp. All rights reserved.
+//
+
+#import "BRNotificationTimeViewController.h"
+
+@interface BRNotificationTimeViewController ()
+
+@end
+
+@implementation BRNotificationTimeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
