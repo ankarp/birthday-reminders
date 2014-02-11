743c26987a11b5fae58c97fee6926bfc1f7c25a7
Anatoly Karp <ankarp1@gmail.com>
2014-02-10 23:19:05 -0800

    add BRDBirthday entity

---
+++ NewBirthdayReminder/data/BRDBirthday.m
@@ -0,0 +1,27 @@
+//
+//  BRDBirthday.m
+//  NewBirthdayReminder
+//
+//  Created by Jennnifer Karp on 2/10/14.
+//  Copyright (c) 2014 tolikarp. All rights reserved.
+//
+
+#import "BRDBirthday.h"
+
+
+@implementation BRDBirthday
+
+@dynamic birthDay;
+@dynamic addressBookID;
+@dynamic birthMonth;
+@dynamic birthYear;
+@dynamic facebookID;
+@dynamic imageData;
+@dynamic name;
+@dynamic nextBirthday;
+@dynamic nextBirthdayAge;
+@dynamic notes;
+@dynamic picURL;
+@dynamic uid;
+
+@end
